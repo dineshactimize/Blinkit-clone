be9ede45ee4f61ee75f0b93167b0e2353f910227
dineshactimize <dinesh.actimize@gmail.com>
2026-02-17 09:42:41 +0530

    server

--- server/server.js
+++ server/server.js
@@ -20,11 +20,6 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Fallback for React routing
-app.get('/*splat', (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, '../client/build', 'index.html')
-  );
-});
 
 app.use(cors());
 app.use(express.json());
@@ -53,6 +48,11 @@ app.use('/api/notifications', require('./routes/notificationRoutes'));
 
 app.use(errorHandler);
 
+app.get('/*splat', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, '../client/build', 'index.html')
+  );
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
   console.log(`Server started on port ${port.yellow.bold}`)
